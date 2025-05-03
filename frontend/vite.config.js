55fb62d29791bb66eb0c1a2045f6529a8df9b7d5
Pranjal Maithani <pranjalmaithani81822@gmail.com>
2025-05-03 19:03:19 +0530

    Added frontend setup and initial files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,14 @@
+// vite.config.js
 import { defineConfig } from 'vite'
-import path from "path"
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+  plugins: [react()],
+  build: {
+    outDir: 'dist'
   },
+  server: {
+    port: 3000
+  },
+  base: './'
 })
